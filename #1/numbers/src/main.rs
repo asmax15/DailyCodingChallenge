dacb074293e51cee30ba17f0c334562d68325f99
asmax15 <max.aschmann@gmail.com>
2022-01-12 20:20:00 +0100

    Change mutability of some variables in challenge #1

--- #1/numbers/src/main.rs
+++ #1/numbers/src/main.rs
@@ -1,14 +1,14 @@
 use rand::Rng;
 
 fn main() {
-    let mut list = [10, 15, 3, 7];
+    let list = [10, 15, 3, 7];
     let k = 17;
 
     loop {
-        let mut rnd = rand::thread_rng().gen_range(0..list.len());
-        let mut rnd2 = rand::thread_rng().gen_range(0..list.len());
+        let rnd = rand::thread_rng().gen_range(0..list.len());
+        let rnd2 = rand::thread_rng().gen_range(0..list.len());
 
-        let mut result = &list[rnd] + &list[rnd2];
+        let result = &list[rnd] + &list[rnd2];
         
 
         if result == k {
